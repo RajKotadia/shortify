992ab4e8528145a9c141357931c1da094d86eee7
RajKotadia <rajkotadia15@gmail.com>
2020-10-20 15:27:06 +0530

    :bug: store urls as it is in db

--- models/ShortUrl.js
+++ models/ShortUrl.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose');
-const shortid = require('shortid');
+const mongoose = require("mongoose");
+const shortid = require("shortid");
 
 const Schema = mongoose.Schema;
 
-const ShortUrlSchema = new Schema({
-    url: {
-        type: String,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        required: true,
-    },
-    urlCode: {
-        type: String,
-        default: shortid.generate,
-    }
-}, {
-    timestamps: true
-});
+const ShortUrlSchema = new Schema(
+	{
+		url: {
+			type: String,
+			unique: true,
+			trim: true,
+			required: true,
+		},
+		urlCode: {
+			type: String,
+			default: shortid.generate,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-const ShortUrl = mongoose.model('ShortUrl', ShortUrlSchema);
+const ShortUrl = mongoose.model("ShortUrl", ShortUrlSchema);
 
 module.exports = ShortUrl;
